c651865fdb01d9c8d82c335f6a756bc760803a82
Osmani Rosado Falcon <osmanir@uclv.cu>
2020-04-03 11:46:45 -0400

    Register City in admin site.

--- web/dvdrental/admin.py
+++ web/dvdrental/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 
-from .models import Country
+from .models import Country, City
 
 admin.site.register(Country)
+
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = ['city', 'country']
+
+
+admin.site.register(City, CityAdmin)

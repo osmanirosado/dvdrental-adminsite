3be440acd267cb4e502a65ff647404f953e45cad
Osmani Rosado Falcon <osmanir@uclv.cu>
2020-04-03 11:59:17 -0400

    Show City related objects to Country edit view.

--- web/dvdrental/admin.py
+++ web/dvdrental/admin.py
@@ -2,7 +2,17 @@ from django.contrib import admin
 
 from .models import Country, City
 
-admin.site.register(Country)
+
+class CityInline(admin.StackedInline):
+    model = City
+    extra = 3
+
+
+class CountryAdmin(admin.ModelAdmin):
+    inlines = [CityInline]
+
+
+admin.site.register(Country, CountryAdmin)
 
 
 class CityAdmin(admin.ModelAdmin):

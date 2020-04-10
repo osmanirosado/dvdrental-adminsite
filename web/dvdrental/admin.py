4301f3168d29094f670ca86894bbd6bd3efb4d78
Osmani Rosado Falcon <osmanir@uclv.cu>
2020-04-09 22:05:29 -0400

    Register ActorInfo database materialized view in admin.

--- web/dvdrental/admin.py
+++ web/dvdrental/admin.py
@@ -44,3 +44,22 @@ class FilmAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Film, FilmAdmin)
+
+
+class ActorInfoAdmin(admin.ModelAdmin):
+    list_display = ['actor_id', 'first_name', 'last_name', 'film_info']
+    list_per_page = 25
+    search_fields = ['first_name', 'last_name']
+
+    # actor_info is a data base materialized view, disable add, change and delete permissions
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+admin.site.register(ActorInfo, ActorInfoAdmin)

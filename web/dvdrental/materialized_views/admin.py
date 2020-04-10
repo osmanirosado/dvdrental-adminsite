616b729427f470358a192f29c8810376286b5d62
Osmani Rosado Falcon <osmanir@uclv.cu>
2020-04-09 23:23:30 -0400

    Create a super class for model admin materialized views.

--- web/dvdrental/materialized_views/admin.py
+++ web/dvdrental/materialized_views/admin.py
@@ -3,12 +3,8 @@ from django.contrib import admin
 from .models import *
 
 
-class ActorInfoAdmin(admin.ModelAdmin):
-    list_display = ['actor_id', 'first_name', 'last_name', 'film_info']
-    list_per_page = 25
-    search_fields = ['first_name', 'last_name']
-
-    # actor_info is a data base materialized view, disable add, change and delete permissions
+class MaterializedViewModelAdmin(admin.ModelAdmin):
+    # Disable add, change and delete permissions for data base materialized views
     def has_add_permission(self, request):
         return False
 
@@ -17,6 +13,12 @@ class ActorInfoAdmin(admin.ModelAdmin):
 
     def has_delete_permission(self, request, obj=None):
         return False
+
+
+class ActorInfoAdmin(MaterializedViewModelAdmin):
+    list_display = ['actor_id', 'first_name', 'last_name', 'film_info']
+    list_per_page = 25
+    search_fields = ['first_name', 'last_name']
 
 
 admin.site.register(ActorInfo, ActorInfoAdmin)

78c913ed5fc476f9a2a0fb609c9370b87b465ca9
Osmani Rosado Falcon <osmanir@uclv.cu>
2020-04-10 22:52:05 -0400

    Rename materialized_views model to database_views.

--- web/website/settings.py
+++ web/website/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'dvdrental',
-    'dvdrental.materialized_views',
+    'dvdrental.database_views',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
